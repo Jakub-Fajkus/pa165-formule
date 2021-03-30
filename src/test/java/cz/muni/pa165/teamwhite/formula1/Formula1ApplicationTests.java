f58f7d935b6229373499bc999ecd0eba0f61a9e4
jakub.fajkus@gmail.com <jakub.fajkus@gmail.com>
2021-03-30 16:29:21 +0200

    Init spring application
    Created using spring boot. Additional dependencies added.

---
+++ src/test/java/cz/muni/pa165/teamwhite/formula1/Formula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.pa165.teamwhite.formula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
