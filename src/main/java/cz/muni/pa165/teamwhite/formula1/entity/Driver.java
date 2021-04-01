6871509d58904d6ebbfa230d7dd58614e3efb7f3
tomass <456478@mail.muni.cz>
2021-04-01 18:34:11 +0200

    ADD Driver entity

---
+++ src/main/java/cz/muni/pa165/teamwhite/formula1/entity/Driver.java
@@ -0,0 +1,159 @@
+package cz.muni.pa165.teamwhite.formula1.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+
+/**
+ * @author Tomas Sedlacek
+ */
+
+@Entity
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Car car;
+
+    @Column(nullable = false)
+    @NotNull
+    private String name;
+
+    @Column(nullable = false)
+    @NotNull
+    private String surname;
+
+    @Column(nullable = false)
+    @NotNull
+    private String nationality;
+
+    @Column(nullable = false)
+    @NotNull
+    private boolean isAgresive;
+
+    @Column(nullable = false)
+    @NotNull
+    private int wetDriving;
+
+    @Column(nullable = false)
+    @NotNull
+    private int reactions;
+
+
+    public Driver() {}
+
+    public Driver(Long id) {
+        this.id = id;
+    }
+
+    public Driver(Long id, Car car, String name, String surname, String nationality,
+                  boolean isAgresive, int wetDriving, int reactions) {
+        this.id = id;
+        this.car = car;
+        this.name = name;
+        this.surname = surname;
+        this.nationality = nationality;
+        this.isAgresive = isAgresive;
+        this.wetDriving = wetDriving;
+        this.reactions = reactions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public boolean isAgresive() {
+        return isAgresive;
+    }
+
+    public int getWetDriving() {
+        return wetDriving;
+    }
+
+    public int getReactions() {
+        return reactions;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setNationaliry(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setAgresive(boolean agresive) {
+        isAgresive = agresive;
+    }
+
+    public void setWetDriving(int wetDriving) {
+        this.wetDriving = wetDriving;
+    }
+
+    public void setReactions(int reactions) {
+        this.reactions = reactions;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id=" + id +
+                ", car=" + car +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", isAgresive=" + isAgresive +
+                ", wetDriving=" + wetDriving +
+                ", reactions=" + reactions +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Driver)) return false;
+        Driver driver = (Driver) o;
+        return getName().equals(driver.getName()) && getSurname().equals(driver.getSurname()) && getNationality().equals(driver.getNationality());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname(), getNationality());
+    }
+}
