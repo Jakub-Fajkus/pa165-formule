4807a053394b9f67ca93dd01f2cb8b48b5a1218d
tomasedl <53978723+tomasedl@users.noreply.github.com>
2021-04-04 18:04:17 +0200

    ADD userDao interface + impl (#8)
    * ADD userDao interface + impl
    
    
    Co-authored-by: tomass <456478@mail.muni.cz>

---
+++ src/main/java/cz/muni/pa165/teamwhite/formula1/dao/UserDaoImpl.java
@@ -0,0 +1,51 @@
+package cz.muni.pa165.teamwhite.formula1.dao;
+
+
+import cz.muni.pa165.teamwhite.formula1.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author Tomas Sedlacek
+ */
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return em.find(User.class, id);
+    }
+
+    @Override
+    public User findByLogin(String userLogin) {
+        return em.createQuery("select u from User u where u.login = :login", User.class)
+                .setParameter("login", userLogin).getSingleResult();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public void update(User user) {
+        em.merge(user);
+    }
+
+    @Override
+    public void remove(User user) {
+        user = this.findById(user.getId());
+        em.remove(user);
+    }
+}
