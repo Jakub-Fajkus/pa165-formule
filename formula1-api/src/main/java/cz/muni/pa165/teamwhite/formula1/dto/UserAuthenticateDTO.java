a41721b0a05113ce4f045da80c24aab4ec023e36
jakub.fajkus@gmail.com <jakub.fajkus@gmail.com>
2021-04-24 13:07:46 +0200

    UserFacade
    Co-authored-by: Karolina Hecova <karolina.hecova@gmail.com>

---
+++ formula1-api/src/main/java/cz/muni/pa165/teamwhite/formula1/dto/UserAuthenticateDTO.java
@@ -0,0 +1,35 @@
+package cz.muni.pa165.teamwhite.formula1.dto;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Karolina Hecova
+ */
+public class UserAuthenticateDTO
+{
+    @NotNull
+    private String login;
+
+    @NotNull
+    private String password;
+
+    public String getLogin()
+    {
+        return login;
+    }
+
+    public void setUserId(String login)
+    {
+        this.login = login;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
---
+++ formula1-api/src/main/java/cz/muni/pa165/teamwhite/formula1/facade/UserFacade.java
@@ -0,0 +1,53 @@
+package cz.muni.pa165.teamwhite.formula1.facade;
+
+import cz.muni.pa165.teamwhite.formula1.dto.UserDTO;
+import cz.muni.pa165.teamwhite.formula1.dto.UserAuthenticateDTO;
+
+import java.util.List;
+
+/**
+ * @author Karolina Hecova
+ */
+public interface UserFacade {
+
+	/**
+	 * Gets list of all users.
+	 * @return list of all users
+	 */
+	List<UserDTO> getAllUsers();
+
+	/**
+	 * Creates new user with the given unencrypted password.
+	 * @param userDTO new user to be created
+	 * @param unencryptedPassword unencrypted password
+	 * @return id of the new user
+	 */
+	Long createUser(UserDTO userDTO, String unencryptedPassword);
+
+	/**
+	 * Deletes user by given id.
+	 * @param userId id of the user to be deleted
+	 */
+	void deleteUser(Long userId);
+
+	/**
+	 * Gets user by given id.
+	 * @param userId id of the wanted user
+	 * @return user
+	 */
+	UserDTO getUserById(Long userId);
+
+	/**
+	 * Try to authenticate a user. Return true only if the hashed password matches the records.
+	 * @parem user to be created
+	 * @return true if the user was created, otherwise false
+	 */
+	boolean authenticate(UserAuthenticateDTO user);
+
+	/**
+	 * Check if the given user is manager.
+	 * @param user to be tested as manager
+	 * @return true if the given user is manager, otherwise false
+	 */
+	boolean isManager(UserDTO user);
+}
