d4ea369506bfcbdbb16c48fe6c198a4cee67d714
jakub.fajkus@gmail.com <jakub.fajkus@gmail.com>
2021-04-24 13:05:22 +0200

    ComponentDTO
    Co-authored-by: tomass <456478@mail.muni.cz>

---
+++ formula1-api/src/main/java/cz/muni/pa165/teamwhite/formula1/dto/ComponentDTO.java
@@ -0,0 +1,72 @@
+package cz.muni.pa165.teamwhite.formula1.dto;
+
+import cz.muni.pa165.teamwhite.formula1.enums.ComponentType;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+/**
+ * @author Tomas Sedlacek
+ */
+
+public class ComponentDTO {
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private CarDTO car;
+
+    @NotNull
+    private ComponentType type;
+
+    public ComponentDTO(ComponentType ctype, String name, CarDTO car) {
+        this.type = ctype;
+        this.name = name;
+        this.car = car;
+    }
+
+    public ComponentDTO(Long id, ComponentType ctype, String name, CarDTO car) {
+        this(ctype, name, car);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public CarDTO getCar() {
+        return car;
+    }
+
+    public ComponentType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "ComponentDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", car=" + car.getName() +
+                ", type=" + type +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ComponentDTO)) return false;
+        ComponentDTO component = (ComponentDTO) o;
+        return getName().equals(component.getName()) && getType() == component.getType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getType());
+    }
+}
---
+++ formula1-api/src/main/java/cz/muni/pa165/teamwhite/formula1/enums/ComponentType.java
@@ -0,0 +1,12 @@
+package cz.muni.pa165.teamwhite.formula1.enums;
+
+/**
+ * @author Tomas Sedlace
+ */
+public enum ComponentType {
+    ENGINE,
+    SUSPENSION,
+    TRANSMISSION,
+    RIMS,
+    REARSPOILER
+}
