ddf2073e0603a1724de09e5950c7b976c0a69a01
jakub.fajkus@gmail.com <jakub.fajkus@gmail.com>
2021-04-24 13:08:43 +0200

    DriverFacade
    Co-authored-by: jandrlik <jandrlik@redhat.com>

---
+++ formula1-api/src/main/java/cz/muni/pa165/teamwhite/formula1/facade/DriverFacade.java
@@ -0,0 +1,37 @@
+package cz.muni.pa165.teamwhite.formula1.facade;
+
+
+import cz.muni.pa165.teamwhite.formula1.dto.DriverDTO;
+
+import java.util.List;
+
+/**
+ * @author Jiri Andrlik
+ */
+public interface DriverFacade {
+
+	/**
+	 * @return list of all drivers
+	 */
+	List<DriverDTO> getAllDrivers();
+
+	/**
+	 *
+	 * @param driverDTO driver to be added to the database
+	 * @return id of created driver
+	 */
+	Long createDriver(DriverDTO driverDTO);
+
+	/**
+	 * deletes a driver from the database
+	 * @param driverId - id of the driver to be deleted
+	 */
+	void deleteDriver(Long driverId);
+
+	/**
+	 * returns single driver from the database
+	 * @param driverId - id of the driver
+	 * @return DriverDTO of the driver
+	 */
+	DriverDTO getDriverById(Long driverId);
+}
