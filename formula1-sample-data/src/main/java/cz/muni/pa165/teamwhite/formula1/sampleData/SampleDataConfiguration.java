1f6f1d2b88dfa0a69822a5c7ba6f7ca2f9998471
jakub.fajkus@gmail.com <jakub.fajkus@gmail.com>
2021-05-10 13:16:24 +0200

    Sample data

---
+++ formula1-sample-data/src/main/java/cz/muni/pa165/teamwhite/formula1/sampleData/SampleDataConfiguration.java
@@ -0,0 +1,25 @@
+package cz.muni.pa165.teamwhite.formula1.sampleData;
+
+import cz.muni.pa165.teamwhite.formula1.service.ServiceConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Configuration
+@Import(ServiceConfiguration.class)
+@ComponentScan(basePackageClasses = {SampleDataLoadingFacadeImpl.class})
+public class SampleDataConfiguration {
+
+
+    @Autowired
+    SampleDataLoadingFacade sampleDataLoadingFacade;
+
+    @PostConstruct
+    public void dataLoading() throws IOException {
+        sampleDataLoadingFacade.loadData();
+    }
+}
---
+++ formula1-sample-data/src/main/java/cz/muni/pa165/teamwhite/formula1/sampleData/SampleDataLoadingFacade.java
@@ -0,0 +1,7 @@
+package cz.muni.pa165.teamwhite.formula1.sampleData;
+
+import java.io.IOException;
+
+public interface SampleDataLoadingFacade {
+    void loadData() throws IOException;
+}
