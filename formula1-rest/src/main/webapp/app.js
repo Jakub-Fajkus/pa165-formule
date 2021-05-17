9e750546e5bb3ad8b9dccf36d59344c2e382ab69
jakub.fajkus@gmail.com <jakub.fajkus@gmail.com>
2021-05-17 12:23:47 +0200

    add admin template

--- formula1-rest/src/main/webapp/app.js
+++ formula1-rest/src/main/webapp/app.js
@@ -57,19 +57,91 @@ export default {
     },
 
     template: `
-        <div id="sidebar">
-            <nav>
-                <button v-on:click="page = ''">Home</button>
-                <template v-for="item, index in pages" key="item.name">
-                    <button @click="tabClicked(index)">
-                        {{ item.name }}
-                    </button>
-                    
-                </template>               
-            </nav><hr>
+
+        <div class="sidebar" data-color="green" data-background-color="white">
+      <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="nav-item active">
+            <a v-on:click="page = ''" class="nav-link" href="#0">
+              <i class="material-icons">Dashboard</i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          
+          <template v-for="item, index in pages" key="item.name">
+              <li class="nav-item ">
+                <a @click="tabClicked(index)" class="nav-link" href="#0">
+                  <i class="material-icons">{{ item.name }}</i>
+                  <p v-on:click="page = ''">{{ item.name }}</p>
+                </a>
+              </li>
+          </template>
+          
+          <!-- your sidebar here -->
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <a class="navbar-brand" href="javascript:;">Dashboard</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:;">
+                  <i class="material-icons">notifications</i> Notifications
+                </a>
+              </li>
+              <!-- your navbar here -->
+            </ul>
+          </div>
         </div>
-        <div id="content">
-            <component :is="page || 'homepage'"></component>
+      </nav>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+          <component :is="page || 'homepage'"></component>
         </div>
+      </div>
+      <footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="https://www.creative-tim.com">
+                  Creative Tim
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+          </div>
+          <!-- your footer here -->
+        </div>
+      </footer>
+    </div>
+    
+        
     `,
   };
