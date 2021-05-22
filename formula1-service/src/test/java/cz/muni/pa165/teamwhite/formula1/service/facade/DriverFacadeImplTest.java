83aa21cac30b0dec0155411405377c75f2815211
Karolina Hecova <karolina.hecova@gmail.com>
2021-05-22 20:58:50 +0200

    add @BeforeMethod to tests

--- formula1-service/src/test/java/cz/muni/pa165/teamwhite/formula1/service/facade/DriverFacadeImplTest.java
+++ formula1-service/src/test/java/cz/muni/pa165/teamwhite/formula1/service/facade/DriverFacadeImplTest.java
@@ -4,13 +4,16 @@ import cz.muni.pa165.teamwhite.formula1.dto.CarDTO;
 import cz.muni.pa165.teamwhite.formula1.dto.DriverDTO;
 import cz.muni.pa165.teamwhite.formula1.facade.CarFacade;
 import cz.muni.pa165.teamwhite.formula1.facade.DriverFacade;
+import cz.muni.pa165.teamwhite.formula1.persistence.entity.Driver;
 import cz.muni.pa165.teamwhite.formula1.service.ServiceConfiguration;
 import org.junit.Assert;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.transaction.Transactional;
@@ -26,12 +29,15 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private DriverFacade driverFacade;
 
-    @Autowired
-    private CarFacade carFacade;
+    private DriverDTO driverDTO;
+
+    @BeforeMethod
+    public void setUp() {
+        driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "Czech", true, 9, 9);
+    }
 
     @Test
     public void testCreateAndThenUpdateOnlyName() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "Czech", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, null, "Michael", null, null, null, null, null));
 
@@ -41,7 +47,6 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testCreateAndThenUpdateOnlySurname() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "Czech", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, null, null, "Schumacher", null, null, null, null));
 
@@ -51,7 +56,6 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testCreateAndThenUpdateOnlyNationality() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "FI", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, null, null, null, "Czech", null, null, null));
 
@@ -61,7 +65,6 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testCreateAndThenUpdateOnlyAggressivity() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "FI", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, null, null, null, null, false, null, null));
 
@@ -71,7 +74,6 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testCreateAndThenUpdateOnlyWetdriving() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "FI", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, null, null, null, null, null, 2, null));
 
@@ -81,7 +83,6 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testCreateAndThenUpdateOnlyReactions() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "FI", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, null, null, null, null, null, null, 2));
 
@@ -91,7 +92,6 @@ public class DriverFacadeImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testCreateAndThenUpdateOnlyCar() {
-        DriverDTO driverDTO = new DriverDTO(null, "Valtteri", "Bottas", "Czech", true, 9, 9);
         Long id = driverFacade.createDriver(driverDTO);
         DriverDTO updated = driverFacade.update(new DriverDTO(id, new CarDTO("audi", driverDTO, Set.of()), null, null, null, null, null, null));
 
